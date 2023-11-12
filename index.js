53d6256685ebec4279ccf559384832fe39aa9ea2
Tianny Vinarao <111848986+Shinzer@users.noreply.github.com>
2023-11-12 20:26:51 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;   
+var randomImageSource1 = "dice" + randomNumber1 + ".png";   
+$('img')[0].setAttribute("src", randomImageSource1); 
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
+$('img')[1].setAttribute("src", randomImageSource2);
+
+
+if (randomNumber1 > randomNumber2) {
+    $('h1').innerHTML = "🚩Player 1 Wins";
+}
+else if (randomNumber2 > randomNumber1) {
+    $('h1').innerHTML = "Player 2 Wins🚩"
+}
+else {
+  $('h1').innerHTML = "Draw!"
+}
